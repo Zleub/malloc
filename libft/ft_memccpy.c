255a8b605e8c44a58e793eebeb75b3505527276f
Zleub <debray.arnaud@gmail.com>
2016-12-20 20:58:27 +0100

    libft

---
+++ libft/ft_memccpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/27 00:23:42 by adebray           #+#    #+#             */
+/*   Updated: 2015/09/21 19:31:09 by adebray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
+{
+	unsigned char	*tmp1;
+	unsigned char	*tmp2;
+
+	tmp1 = (unsigned char*)s1;
+	tmp2 = (unsigned char*)s2;
+	if (c < 0)
+		c *= -1;
+	while (n--)
+	{
+		if (*tmp2 == c)
+		{
+			*tmp1++ = *tmp2++;
+			return (s1);
+		}
+		*tmp1++ = *tmp2++;
+	}
+	return (NULL);
+}
