3b12cc57bcc18dee35f25ca9e13f6df308cdbe56
Zleub <debray.arnaud@gmail.com>
2016-12-22 02:44:20 +0100

    inf loop

--- main.c
+++ main.c
@@ -1,54 +1,55 @@
 #include <malloc.h>
 
-// int main(void) {
-
-// 	SPRINTF("%zu\n", sizeof(size_t));
-// 	int i = 0;
-// 	while (i < 10) {
-// 		char test[] = "arno";
-// 		int i = rand() % 500 + 5;
-// 		SPRINTF("allocate %d\n", i);
-// 		void *p = malloc( i );
-
-// 		strcpy(p, test);
-// 	}
-// 	return (0);
-// }
-
-#define TESTSIZE 100
-
-void show_alloc_mem();
 int main(void) {
 
-	void *array[100] ={0};
+	SPRINTF("%zu\n", sizeof(size_t));
+	int i = 0;
+	while (i < 10) {
+		char test[] = "arno";
+		int i = rand() % 50 + 5;
+		SPRINTF("allocate %d\n", i);
+		void *p = malloc( i );
 
-	int i = 1;
-	int j = 0;
-
-	// debug();
-
-	while (i < TESTSIZE) {
-		if ((i + 1) % 4 == 0) {
-			int j = rand() % 50 + 5;
-			SPRINTF("allocate %d\n", j);
-			void *p = malloc( j );
-			array[i] = p;
-			strcpy(p, "test");
-		}
-		else if ((i + 1) % 3 == 0) {
-			int j = rand() % i;
-			SPRINTF("free: %d %p\n", j, array[j])
-			free(array[j]);
-		}
-		else {
-			int j = rand() % 5000 + 5;
-			SPRINTF("allocate %d\n", j);
-			void *p = malloc( j );
-			array[i] = p;
-			strcpy(p, "test");
-		}
-
+		strcpy(p, test);
 		show_alloc_mem();
-		i += 1;
 	}
+	return (0);
 }
+
+// #define TESTSIZE 100
+
+// void show_alloc_mem();
+// int main(void) {
+
+// 	void *array[100] ={0};
+
+// 	int i = 1;
+// 	int j = 0;
+
+// 	// debug();
+
+// 	while (i < TESTSIZE) {
+// 		if ((i + 1) % 4 == 0) {
+// 			int j = rand() % 50 + 5;
+// 			SPRINTF("allocate %d\n", j);
+// 			void *p = malloc( j );
+// 			array[i] = p;
+// 			strcpy(p, "test");
+// 		}
+// 		else if ((i + 1) % 3 == 0) {
+// 			int j = rand() % i;
+// 			SPRINTF("free: %d %p\n", j, array[j])
+// 			free(array[j]);
+// 		}
+// 		else {
+// 			int j = rand() % 5000 + 5;
+// 			SPRINTF("allocate %d\n", j);
+// 			void *p = malloc( j );
+// 			array[i] = p;
+// 			strcpy(p, "test");
+// 		}
+
+// 		show_alloc_mem();
+// 		i += 1;
+// 	}
+// }
